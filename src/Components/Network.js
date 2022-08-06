8d9d0e54f8b08d4d88ad25041337d0a3a26fd27e
loveragabriel <loveragabriel20@gmail.com>
2022-08-06 18:27:00 -0300

    network component added

---
+++ src/Components/Network.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export const Network = () => {
+  return (
+    <div>
+        <div id="network-icons">
+        <i className="fa-brands fa-linkedin"></i>        
+        <i className="fa-brands fa-github"></i>     
+         </div>
+    </div>
+  )
+}
